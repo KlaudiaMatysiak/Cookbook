9ae5f2b1c50ee6024b5fcfb885527468936dcd06
KlaudiaMatysiak <klatysiak@gmail.com>
2022-04-09 12:14:06 +0000

    Add app.py file

---
+++ app.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask
+if os.path.exists("env.py"):
+    import env
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    """function that prints hello"""
+    return "Hello"
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
