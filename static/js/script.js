5012f94380f73bfee922e2846eb457f7804e6344
KlaudiaMatysiak <klatysiak@gmail.com>
2022-04-11 11:58:49 +0000

    Add comment for jshint and tidy up the code

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,9 @@
+/* jshint esversion: 8, jquary: true */
 $(document).ready(function(){
+    $(".dropdown-trigger").dropdown({
+      coverTrigger: false,
+      inDuration: 800
+    });
     $('.modal').modal();
   });
   
