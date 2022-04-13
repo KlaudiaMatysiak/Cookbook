fe91f92de3f1d7a8061c7719837331f2744c4994
KlaudiaMatysiak <klatysiak@gmail.com>
2022-04-13 19:25:20 +0000

    Add functionality for the search recipe

--- app.py
+++ app.py
@@ -31,6 +31,14 @@ def get_recipes():
     return render_template("recipes.html", recipes=recipes)
 
 
+@app.route("/search", methods=["GET", "POST"])
+def search():
+    """Searching for specific recipe"""
+    query = request.form.get("query")
+    recipes = list(mongo.db.recipes.find({"$text": {"$search": query}}))
+    return render_template("recipes.html", recipes=recipes)
+
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """register user"""
