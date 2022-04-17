4358ca01d970fbd764d5bdd758b4018f9939fb90
KlaudiaMatysiak <klatysiak@gmail.com>
2022-04-17 00:10:18 +0000

    Change debug from True to False

--- app.py
+++ app.py
@@ -181,4 +181,4 @@ def show_recipe(recipe_id):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
